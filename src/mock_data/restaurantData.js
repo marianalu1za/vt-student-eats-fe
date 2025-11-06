74202f66dc9f22ce18e1797b86d6750a5a56f8b2
Sheng <zuyishinlala@gmail.com>
2025-11-05 23:03:40 -0500

    feat: add chipotle restaurant with real images

--- src/mock_data/restaurantData.js
+++ src/mock_data/restaurantData.js
@@ -1,60 +1,70 @@
 // Mock restaurant data
 export const restaurants = [
   {
+    id: 8,
+    name: 'Chipotle',
+    rating: 4.4,
+    tags: ['Burritos', 'Delivery', 'Group Orders'],
+    image: '🍕',
+    images: ['/images/chipotle/Chipotle.jpeg', '/images/chipotle/burrito.png']
+
+  },
+  {
     id: 1,
     name: 'Campus Bistro',
     rating: 4.5,
     tags: ['Fast Food', 'Budget Friendly', 'Student Discount'],
-    image: '🍕'
+    image: '🍕',
+    images: ['🍕', '🍔', '🍟']
   },
   {
     id: 2,
     name: 'The Corner Cafe',
     rating: 4.8,
     tags: ['Coffee', 'Breakfast', 'Vegetarian Options'],
-    image: '☕'
+    image: '☕',
+    images: ['☕', '🥐', '🥞']
   },
   {
     id: 3,
     name: 'Noodles & More',
     rating: 4.3,
     tags: ['Asian', 'Noodles', 'Quick Service'],
-    image: '🍜'
+    image: '🍜',
+    images: ['🍜', '🥟', '🍱']
   },
   {
     id: 4,
     name: 'Burger Junction',
     rating: 4.6,
     tags: ['Burgers', 'Late Night', 'Student Discount'],
-    image: '🍔'
+    image: '🍔',
+    images: ['🍔', '🍟', '🥤']
   },
   {
     id: 5,
     name: 'Fresh Salads Co',
     rating: 4.7,
     tags: ['Healthy', 'Salads', 'Vegetarian'],
-    image: '🥗'
+    image: '🥗',
+    images: ['🥗', '🥑', '🥒']
   },
   {
     id: 6,
     name: 'Pizza Place',
     rating: 4.4,
     tags: ['Pizza', 'Delivery', 'Group Orders'],
-    image: '🍕'
+    image: '🍕',
+    images: ['🍕', '🍕', '🍕']
   },
   {
     id: 7,
     name: 'Cafe Shop',
     rating: 4.7,
     tags: ['Healthy', 'Salads', 'Vegetarian'],
-    image: '🥗'
+    image: '🥗',
+    images: ['🥗', '☕', '🥐']
   },
-  {
-    id: 8,
-    name: 'Chipotle',
-    rating: 4.4,
-    tags: ['Burritos', 'Delivery', 'Group Orders'],
-    image: '🍕'
-  }
+  
 ]
 
