a9418b0f7bc0d0703bd3aed57fc3e7bec6b7dba8
Sheng <zuyishinlala@gmail.com>
2025-11-18 00:23:48 -0500

    feat: add reviews mock data

---
+++ src/mock_data/reviews.js
@@ -0,0 +1,31 @@
+// Mock reviews data
+export const mockReviews = [
+  {
+    id: 1,
+    userName: 'Bunty G',
+    userInitial: 'B',
+    contributions: 1,
+    orderDate: '2025-07-18',
+    reviewText: 'Hey this place made my day Creamy, Comforting & Satisfying! 🍝 I tried two pasta dishes from Pasta Stop today the first Pen...',
+    rating: 3
+  },
+  {
+    id: 2,
+    userName: 'Jhoselin R',
+    userInitial: 'J',
+    contributions: 1,
+    orderDate: '2025-07-13',
+    reviewText: 'Absolutely delicious! I ordered the grilled salmon, french fries, and a slice of chocolate cake, and everything was spot on. The...',
+    rating: 2.9
+  },
+  {
+    id: 3,
+    userName: 'Abigail S',
+    userInitial: 'A',
+    contributions: 81,
+    orderDate: '2025-11-11',
+    reviewText: 'This place is amazing. I\'ve ordered multiple times and it\'s always perfect - delicious, packed well, huge portions...',
+    rating: 5
+  }
+]
+
